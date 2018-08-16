026d6aed1fdd6a5c37e0ddd6dc9e0547fd50f731
Topik <topik@rayyic.xyz>
2018-08-16 16:47:39 -0500

    Added the navbar and some files

---
+++ navbar.js
@@ -0,0 +1,15 @@
+var navigation = responsiveNav(".nav-collapse", {
+  animate: true,
+  transition: 284,
+  label: "Menu",
+  insert: "before",
+  customToggle: "",
+  closeOnNavClick: false,
+  openPos: "relative",
+  navClass: "nav-collapse",
+  navActiveClass: "js-nav-active",
+  jsClass: "js",
+  init: function(){},
+  open: function(){},
+  close: function(){}
+});
