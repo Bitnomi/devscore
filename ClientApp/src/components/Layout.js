fb063c136a4d6619822317a76617fc6b7ca4ece0
Tapudp <divyeshpar2496@gmail.com>
2018-08-23 19:44:49 +0530

    added caraousel with ant design yet to load pictures

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Col, Grid, Row } from 'react-bootstrap';
 import { NavMenu } from './NavMenu';
+import 'antd/dist/antd.css';
+import { Carousel } from 'antd';
+import './carousel.css'
 
 export class Layout extends Component {
   displayName = Layout.name
@@ -10,9 +13,15 @@ export class Layout extends Component {
       <Grid fluid>
         <Row>
           <Col sm={3}>
-            <NavMenu />
+            <NavMenu/>
           </Col>
-          <Col sm={9}>
+          <Col sm={6}>
+            <Carousel>
+                <div><img src="B:\Shall\work\devscore\ClientApp\public\2938.jpg" alt=""/></div>
+                <div><h3>2</h3></div>
+                <div><h3>3</h3></div>
+                <div><h3>4</h3></div>
+            </Carousel>
             {this.props.children}
           </Col>
         </Row>
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -3,39 +3,85 @@ import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
+import 'antd/dist/antd.css';
+import { Menu, Icon } from 'antd';
+
+//const SubMenu = Menu.SubMenu;
+//const MenuItemGroup = Menu.ItemGroup;
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
-
+  
+  
   render() {
-    return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to={'/'}>devscore</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
-              </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
+  return (
+  <Navbar fixedTop fluid collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to={'/'}>devscore</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <LinkContainer to={'/'} exact>
+          <NavItem>
+            <Glyphicon glyph='home' /> Home
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={'/counter'}>
+          <NavItem>
+            <Glyphicon glyph='education' /> Counter
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={'/fetchdata'}>
+          <NavItem>
+            <Glyphicon glyph='th-list' /> Fetch data
+          </NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>  
+   )
   }
 }
+    //   state = {
+    //     current: 'home',
+    //   }
+    
+    //   handleClick = (e) => {
+    //     console.log('click', e);
+    //     this.setState({
+    //       current: e.key,
+    //     })
+    //     return (<LinkContainer to={`/${this.state.current}`}>
+    
+    //     </LinkContainer>)
+    //   }
+      
+    //   displayName = NavMenu.name
+    
+    //   render() {
+    //     return (
+    //       <Menu onClick = {this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+    //         <LinkContainer to={'/'} exact>
+    //           <Menu.Item key="home">
+    //             <Icon type= "home" /> Home
+    //           </Menu.Item>
+    //         </LinkContainer>
+            
+    //         <LinkContainer to={'/counter'}>
+    //           <Menu.Item key="counter">
+    //           <Icon type="dashboard" /> counter
+    //           </Menu.Item>
+    //         </LinkContainer>
+              
+    //         <LinkContainer to ={'/fetchdata'}>
+    //           <Menu.Item key="fetchdata">
+    //           <Icon type="setting" /> FetchData
+    //           </Menu.Item>
+    //         </LinkContainer>
+    //       </Menu>
+    //     );
+    //   }
+    // }
+  
